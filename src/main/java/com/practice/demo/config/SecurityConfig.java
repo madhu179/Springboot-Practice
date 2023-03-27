b284248360bab6b418563b56cb480232bae4f0ce
madhu179 <madhubabu232343@gmail.com>
2023-03-27 16:30:51 +0530

    Adding @PreAuthorize to controller methods

--- src/main/java/com/practice/demo/config/SecurityConfig.java
+++ src/main/java/com/practice/demo/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@EnableMethodSecurity enables @PreAuthorize, @PostAuthorize, @PreFilter, @PostFilter by default
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -29,7 +32,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                   .authorizeHttpRequests( (authorize) -> authorize.requestMatchers(HttpMethod.GET, "/api/**")
+                   .authorizeHttpRequests( (authorize) -> authorize.requestMatchers(HttpMethod.GET, "/api/v1/*")
                                 .permitAll().anyRequest().authenticated())
 
 //                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
--- src/main/java/com/practice/demo/controller/WelcomeController.java
+++ src/main/java/com/practice/demo/controller/WelcomeController.java
@@ -1,6 +1,8 @@
 package com.practice.demo.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +13,11 @@ public class WelcomeController {
     @GetMapping("/welcome")
     public String getGreeting(){
         return "Welcome to WestWorld";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/postGreeting")
+    public String postGreeting() {
+        return "Thank you";
     }
 }
