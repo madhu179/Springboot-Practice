534427713be433ff742b98f1fa7e5cc775a08bad
madhu179 <madhubabu232343@gmail.com>
2023-03-27 15:39:50 +0530

    Adding code to store the user details in memory and encrypting the password

--- src/main/java/com/practice/demo/config/SecurityConfig.java
+++ src/main/java/com/practice/demo/config/SecurityConfig.java
@@ -2,12 +2,26 @@ package com.practice.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    //password encryption is necessary for authentication
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 //    auth header needs to be sent in every http request i.e. controller endpoint
 //    auth is done by credential pop up in browser as opposed to the form which is default
@@ -15,8 +29,31 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                   .authorizeHttpRequests( (authorize) -> authorize.requestMatchers(HttpMethod.GET, "/api/**")
+                                .permitAll().anyRequest().authenticated())
+
+//                .authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
+    }
+
+    //storing the user details in memory instead of declaring in app properties
+    //encoding the password
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
