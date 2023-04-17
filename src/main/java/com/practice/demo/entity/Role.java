2a2c989b9b390d49e7c9901f6500c163befd2e4b
madhu179 <madhubabu232343@gmail.com>
2023-04-17 12:44:30 +0530

    Adding code for creating user_role table

---
+++ src/main/java/com/practice/demo/entity/Role.java
@@ -0,0 +1,25 @@
+package com.practice.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+}
